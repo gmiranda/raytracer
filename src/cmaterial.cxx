ee45bb8477ebbeadfa2af8a5af4190dc99f627dc
rutdaun <rutdaun@ce2dd6bc-64f8-f6e2-0175-3f3cbbe2768e>
2006-05-11 12:15:09 +0000

    git-svn-id: https://flashphoto.googlecode.com/svn/raytracer@56 ce2dd6bc-64f8-f6e2-0175-3f3cbbe2768e

---
+++ src/cmaterial.cxx
@@ -0,0 +1,32 @@
+#include "raytracer.h"
+#include "cmaterial.h"
+
+bool CCheckerMaterial::is_white(const VECTOR &loc) const
+{
+  return true;
+}
+
+
+CCheckerMaterial::CCheckerMaterial (CMaterial *white, CMaterial *black, SCALAR size)
+{
+  tile_white = white;
+  tile_black = black;
+  side_size=size;
+}
+
+COLOR CCheckerMaterial::getDiffuseColor(const VECTOR &loc) const
+{
+  if (is_white(loc))
+    return tile_white->getDiffuseColor(loc);
+  else
+    return tile_black->getDiffuseColor(loc);
+}
+
+SCALAR  CCheckerMaterial::getReflectance(const VECTOR &loc) const
+{
+  if (is_white(loc))
+    return tile_white->getReflectance(loc);
+  else
+    return tile_black->getReflectance(loc);
+}
+
---
+++ src/cmaterial.hxx
@@ -0,0 +1,17 @@
+#ifndef _RAY_CMATERIAL_H_
+#define _RAY_CMATERIAL_H_
+
+class CCheckerMaterial : public CMaterial 
+{
+  CMaterial *tile_white;
+  CMaterial *tile_black;
+  SCALAR side_size;
+  
+  bool is_white(const VECTOR &loc) const;
+public:
+  CCheckerMaterial (CMaterial *white, CMaterial *black, SCALAR size);
+  COLOR getDiffuseColor(const VECTOR &loc) const;
+  SCALAR getReflectance(const VECTOR &loc) const;
+};
+
+#endif
